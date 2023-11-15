d9351f993bc22840131f14802672e7ae0cd66c93
Corey Gaspard <cgaspard@gmail.com>
2023-11-15 08:05:43 -0600

    Adding testing capabilities Adding SSL cert support

--- startup.sh
+++ startup.sh
@@ -5,41 +5,10 @@ if [[ -z "$BOOTSTRAP_DELAY" || ! "$BOOTSTRAP_DELAY" =~ ^[0-9]+$ ]]; then
     BOOTSTRAP_DELAY=30
 fi
 
-# Check for the environment variable
-if [ -z "$BOOTSTRAP_SCRIPT" ]; then
-    echo "BOOTSTRAP_SCRIPT is not set. Starting SQL Server."
-    # Just start SQL Server if the environment variable is not present
-    /opt/mssql/bin/sqlservr &
-
-    #if the enviroment variable MemoryLimitsMB exists, then set the max server memory after we confirm sql is running
-    if [ -z "$MSSQL_MAX_MEMORYLIMIT_MB" ]; then
-        echo "MSSQL_MAX_MEMORYLIMIT_MB is not set. Skipping."
-    else
-        # Wait for SQL Server to start up
-        echo "MSSQL_MAX_MEMORYLIMIT_MB is set, waiting for SQL Server to start..."
-        counter=0
-        while ! nc -z localhost 1433; do
-            sleep 1
-            counter=$((counter + 1))
-            if [ $counter -ge 60 ]; then
-                echo "SQL Server did not start within 60 seconds. Exiting."
-                exit 1
-            fi
-        done
-        echo "SQL Server started, waiting 60 seconds before attemptiong to adjust memory limits"
-        sleep 60
-        echo "MSSQL_MAX_MEMORYLIMIT_MB is set. Setting max server memory to $MSSQL_MAX_MEMORYLIMIT_MB MB"
-        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'max server memory', $MSSQL_MAX_MEMORYLIMIT_MB; RECONFIGURE;"
-    fi
-
-    # Keep script running
-    wait
-
-else
-
+start_sql_server() {
     # Start SQL Server in the background
     /opt/mssql/bin/sqlservr &
-
+    
     # Wait for SQL Server to start up
     echo "Waiting for SQL Server to start up..."
     counter=0
@@ -51,15 +20,40 @@ else
             exit 1
         fi
     done
+}
 
+install_certificate() {
+    if [ -n "$CERTIFICATE_FILE" ] && [ -n "$PRIVATE_KEY_FILE" ] && [ -n "$CERTIFICATE_PASSWORD" ]; then
+        echo "Installing certificate from $CERTIFICATE_FILE..."
+        /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "CREATE CERTIFICATE MyServerCert FROM FILE = '$CERTIFICATE_FILE' WITH PRIVATE KEY (FILE = '$PRIVATE_KEY_FILE', DECRYPTION BY PASSWORD = '$CERTIFICATE_PASSWORD');"
+    else
+        echo "Certificate file, private key file, or password not set, skipping certificate installation."
+    fi
+}
+
+# Check for the environment variable
+if [ -z "$BOOTSTRAP_SCRIPT" ]; then
+    echo "BOOTSTRAP_SCRIPT is not set. Starting SQL Server."
+    start_sql_server
+    install_certificate
+    
+    # Handling Memory Limits
     if [ -z "$MSSQL_MAX_MEMORYLIMIT_MB" ]; then
         echo "MSSQL_MAX_MEMORYLIMIT_MB is not set. Skipping."
     else
-        echo "SQL Server started, waiting 60 seconds before attemptiong to adjust memory limits"
+        echo "SQL Server started, waiting 60 seconds before attempting to adjust memory limits"
         sleep 60
-        echo "MSSQL_MAX_MEMORYLIMIT_MB is set. Setting max server memory. $MSSQL_MAX_MEMORYLIMIT_MB MB"
+        echo "MSSQL_MAX_MEMORYLIMIT_MB is set. Setting max server memory to $MSSQL_MAX_MEMORYLIMIT_MB MB"
         /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'max server memory', $MSSQL_MAX_MEMORYLIMIT_MB; RECONFIGURE;"
     fi
+
+    # Keep script running
+    wait
+
+else
+
+    start_sql_server
+    install_certificate
 
     # Check if the bootstrap script has been run before
     if [ -f "/var/opt/mssql/data/bootstrap_started" ]; then
@@ -73,8 +67,17 @@ else
         echo "Waiting for $BOOTSTRAP_DELAY seconds..."
         sleep ${BOOTSTRAP_DELAY}
 
-        # Run the bootstrap script
-        pwsh ${BOOTSTRAP_SCRIPT}
+        # Run the bootstrap script based on its extension
+        if [[ "$BOOTSTRAP_SCRIPT" == *".ps1" ]]; then
+            echo "Running bootstrap script with PowerShell..."
+            pwsh ${BOOTSTRAP_SCRIPT}
+        elif [[ "$BOOTSTRAP_SCRIPT" == *".sh" ]]; then
+            echo "Running bootstrap script with Bash..."
+            bash ${BOOTSTRAP_SCRIPT}
+        else
+            echo "No specific script extension detected for BOOTSTRAP_SCRIPT. Attempting to run directly..."
+            ${BOOTSTRAP_SCRIPT}
+        fi
 
         touch /var/opt/mssql/data/bootstrap_done
 
